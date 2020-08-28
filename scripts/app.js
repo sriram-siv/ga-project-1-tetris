865963f8a345e3671eb6541749f4d232ca66d691
Sriram Sivarajah <sriramsivarajah@gmail.com>
2020-08-28 13:20:12 +0100

    remade block arrays so that they could rotate around the correct point

--- scripts/app.js
+++ scripts/app.js
@@ -8,118 +8,82 @@ function init() {
   const height = 15
 
   let grid
-  
-  const squareBlock = [
+
+  const blocks = [
     [
       [1, 1],
       [1, 1]
-    ]
-  ]
-  const lBlock = [
-    [
-      [ 1, 0],
-      [ 1, 0],
-      [ 1, 1]
     ],
     [
-      [1, 1, 1],
-      [1, 0, 0]
+      [0, 1, 0],
+      [0, 1, 0],
+      [0, 1, 1]
     ],
     [
-      [ 1, 1],
-      [ 0, 1],
-      [ 0, 1]
-    ],
-    [
-      [0, 0, 1],
-      [1, 1, 1]
-    ]
-  ]
-  const jBlock = [
-    [
-      [ 0, 1],
-      [ 0, 1],
-      [ 1, 1]
+      [0, 1, 0],
+      [0, 1, 0],
+      [1, 1, 0]
     ],
     [
       [1, 0, 0],
-      [1, 1, 1]
+      [1, 1, 0],
+      [0, 1, 0]
     ],
     [
-      [ 1, 1],
-      [ 1, 0],
-      [ 1, 0]
+      [0, 1, 0],
+      [1, 1, 0],
+      [1, 0, 0]
     ],
     [
-      [1, 1, 1],
-      [0, 0, 1]
-    ]
-  ]
-  const tBlock = [
-    [
+      [0, 0, 0],
       [1, 1, 1],
       [0, 1, 0]
     ],
     [
-      [0, 1],
-      [1, 1],
-      [0, 1]
-    ],
-    [
-      [0, 1, 0],
-      [1, 1, 1]
-    ],
-    [
-      [1, 0],
-      [1, 1],
-      [1, 0]
-    ]
-  ]
-  const iBlock = [
-    [
-      [1],
-      [1],
-      [1],
-      [1]
-    ],
-    [
-      [1, 1, 1, 1]
-    ],
-    [
-      [1],
-      [1],
-      [1],
-      [1]
-    ],
-    [
-      [1, 1, 1, 1]
-    ]
-  ]
-  const sBlock = [
-    [
-      [0, 1, 1],
-      [1, 1, 0]
-    ],
-    [
-      [1, 0],
-      [1, 1],
-      [0, 1]
-    ]
-  ]
-  const zBlock = [
-    [
-      [1, 1, 0],
-      [0, 1, 1]
-    ],
-    [
-      [0, 1],
-      [1, 1],
-      [1, 0]
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
     ]
   ]
 
+  const pivots = [
+    [
+      // Square
+      [0, 0], [0, 0], [0, 0], [0, 0]
+    ],
+    [
+      // L
+      [1, 0], [0, 1], [0, 0], [2, 0]
+    ],
+    [
+      // J
+      [1, 0], [0, 0], [1, 0], [0, 1]
+    ],
+    [
+      // S
+      [0, 0], [1, 0], [1, 0], [1, 1]
+    ],
+    [
+      // Z
+      [1, 0], [0, 0], [2, 0], [0, 1]
+    ],
+    [
+      // T
+      [0, 1], [1, 0], [1, 0], [1, 0]
+    ],
+    [
+      // I
+      [0, 1], [2, 0], [0, 2], [1, 0]
+    ]
+  ]
+
+  const colors = [ 'Yellow', 'Orange', 'DodgerBlue', 'LimeGreen', 'Red', 'MediumPurple', 'DarkTurquoise' ]
+
   let activeBlock = null
+  let blockId = null
   let blockRotation = 0
+  let clearing = false
 
   // Objects
 
@@ -176,8 +140,9 @@ function init() {
       const cell = document.querySelector(`[data-id='${i}']`)
 
       if (grid[i].state > 0) {
-        const tilePath = `./images/tile_${grid[i].tile}.png`
-        cell.style.background = `url(${tilePath})`
+        // const tilePath = `./images/tile_${grid[i].tile}.png`
+        // cell.style.background = `url(${tilePath})`
+        cell.style.backgroundColor = colors[grid[i].tile]
         cell.style.backgroundSize = 'contain'
       } else {
         cell.style.background = ''
@@ -187,13 +152,12 @@ function init() {
   }
 
   function getBlock() {
-    const blocks = [squareBlock, lBlock, jBlock, tBlock, iBlock, sBlock, zBlock]
     const random = Math.floor(Math.random() * blocks.length)
     const spawnPosition = Math.floor(width / 2 - 1)
 
     let gameOver = false
 
-    blocks[random][0].forEach((line, yIndex) => {
+    blocks[random].forEach((line, yIndex) => {
       line.forEach((cell, xIndex) => {
         const cellToCheck = (yIndex * width) + xIndex + spawnPosition
         if (grid[cellToCheck].state !== 0) {
@@ -207,7 +171,7 @@ function init() {
 
     // Draw block into starting position
     let drawLine = 0
-    blocks[random][0].forEach(line => {
+    blocks[random].forEach(line => {
       line.forEach((cell, i) => {
         grid[drawLine + spawnPosition + i].state = cell
         grid[drawLine + spawnPosition + i].tile = random
@@ -216,29 +180,31 @@ function init() {
       drawLine += width
     })
 
-    activeBlock = blocks[random][0] //*reverseArray
+    activeBlock = blocks[random]
     blockRotation = 0
+    blockId = random
 
     // printBlockState()
   }
 
-  function printBlockState() {
-    // console.clear()
-    activeBlock.forEach(line => console.log(line, '\n')) //*reverseArray
-  }
+  // function printBlockState() {
+  //   // console.clear()
+  //   activeBlock.forEach(line => console.log(line, '\n')) //*reverseArray
+  // }
 
-  function printGridState() {
-    console.clear()
-    for (let i = 0; i < height; i++) {
-      let printLine = ''
-      for (let j = 0; j < width; j++) {
-        printLine += grid[(i * width) + j].state
-      }
-      console.log(printLine)
-    }
-  }
+  // function printGridState() {
+  //   console.clear()
+  //   for (let i = 0; i < height; i++) {
+  //     let printLine = ''
+  //     for (let j = 0; j < width; j++) {
+  //       printLine += grid[(i * width) + j].state
+  //     }
+  //     console.log(printLine)
+  //   }
+  // }
 
   function controlBlock(event) {
+
     switch (event.keyCode) {
       case 37:
         moveBlock('left')
@@ -263,6 +229,7 @@ function init() {
   }
 
   function moveBlock(direction) {
+
 
     // Define variables according to direction
     const touchingEdge = direction === 'left' ? 0 : 9
@@ -306,14 +273,11 @@ function init() {
 
   function rotateBlock(direction) {
 
-    // Test new method
-    activeBlock = rotateArray(activeBlock, direction)
-    activeBlock.forEach(line => console.log(line))
-
-
+    
     // Rotate current block
+    const pivotPoint = pivots[blockId][blockRotation]
+    activeBlock = rotateArray(activeBlock, direction)
     blockRotation = (blockRotation + 1) % 4 //activeBlock.length *reverseArray
-
 
     
     // Find grid location of falling block and remove it
@@ -332,6 +296,14 @@ function init() {
     // Return if no falling block found 
     if (location === null) return
 
+    // Apply pivot point calculation unless it would move out of bounds
+    if (location[0] % width !== 0) {
+      location[0] -= pivotPoint[0]
+    }
+    if (location[1] !== 0) {
+      location[1] -= pivotPoint[1]
+    }
+
     
     // Shift location if it would collide with a boundary or block
     let validMove = false
@@ -345,7 +317,7 @@ function init() {
           const cellToCheck = ((location[1] + yIndex) * width) + (location[0] + xIndex)
 
           // If the cell to place in is occupied by another block, shift the position up
-          if (grid[cellToCheck].state === 2) {
+          if (grid[cellToCheck] === undefined || grid[cellToCheck].state === 2) {
             location[1]--
             validMove = false
           }
@@ -379,8 +351,8 @@ function init() {
     // Is the next line free
     let fall = true
     for (let i = 0; i < grid.length; i++) {
-
-      if (grid[i].state === 1 && (grid[i + width] === undefined || grid[i + width].state === 2)) {
+      // Find falling blocks (normal or in clearing process) that are blocked below
+      if ((grid[i].state === 1 || grid[i].state === 3 ) && (grid[i + width] === undefined || grid[i + width].state === 2)) {
         fall = false
       }
 
@@ -389,31 +361,45 @@ function init() {
     // Move all cells that contain falling blocks down if so
     if (fall) {
       grid = grid.map((cell, i) => {
-
+        // Generate emtpy line for top row
         if (grid[i - width] === undefined) return new CellInfo()
 
+        // Move falling blocks down 1 cell
         if (grid[i - width].state === 1) {
           return new CellInfo(1, grid[i - width].tile)
         }
+        
+        // Move blocks that are falling due to line clearing down 1 cell
+        if (grid[i - width].state === 3) {
+          return new CellInfo(3, grid[i - width].tile)
+        }
+        
+        // Set blocks remain in place
         if (cell.state === 2) {
           return cell
         }
+
+        // Everything else is an empty space
         return new CellInfo()
       })
     } else {
-      // Solidify cells by turning 1 => 2
+
+      // Solidify cells by turning state => 2
       grid = grid.map((cell) => {
-        return cell.state === 1 ? new CellInfo(2, cell.tile) : cell
+        return cell.state === 1 || cell.state === 3
+          ? new CellInfo(2, cell.tile) : cell
       })
-      
+
+      // Clear completed lines
+      clearLines()
 
       // Generate new block
-      getBlock()
+      if (!clearing) getBlock()
+      clearing = false
 
       // Print grid to console
       // printGridState()
 
-      clearLines()
     }
   }
 
@@ -426,15 +412,20 @@ function init() {
       const line = grid.slice(i * width, (i + 1) * width).map(cell => cell.state)
       const clear = line.every(cell => cell === 2)
 
-      // Remove line from grid and move all others down
       if (clear) {
+        clearing = true
+  
+        // Remove line from grid
         for (let j = i * width; j < (i + 1) * width; j++) {
           grid[j] = new CellInfo()
         }
-        // grid.map((cell, i) => {
-        //   if (grid[i].state === 2) grid[i].state = 1
-        // })
-        // dropBlocks()
+
+        // Move all lines above down into empty space
+        grid.map((cell, index) => {
+          if (grid[index].state === 2 && index < i * width) grid[index].state = 3
+        })
+
+        dropBlocks()
       }
     }
 
